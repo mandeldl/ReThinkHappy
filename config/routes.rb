5d19e8802e3f1181443c2edfa9bed04e795b671e
David Mandel <MandelDL@gmail.com>
2015-03-28 16:16:58 -0400

    update routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   resources :activities
 
   get '/signup' => 'users#new'
+
+  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
+  
   post '/users' => 'users#create'
+  post '/login' => 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
