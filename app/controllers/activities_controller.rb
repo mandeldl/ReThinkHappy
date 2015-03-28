4958d5116db6018f8e01c5f87782e8a4c6f9445c
David Mandel <MandelDL@gmail.com>
2015-03-28 16:38:34 -0400

    create sessions and users

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  before_action :require_user
 
   # GET /activities
   # GET /activities.json
