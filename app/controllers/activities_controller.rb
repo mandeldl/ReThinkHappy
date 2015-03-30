ef38f02b001b4d3f3a1b28a776d158c803463dae
David Mandel <MandelDL@gmail.com>
2015-03-29 22:08:59 -0400

    update index and update views and controllers

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -30,7 +30,7 @@ class ActivitiesController < ApplicationController
 
     respond_to do |format|
       if @activity.save
-        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
+        format.html { redirect_to ('/activities'), notice: 'Activity was successfully created.' }
         format.json { render :show, status: :created, location: @activity }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class ActivitiesController < ApplicationController
   def update
     respond_to do |format|
       if @activity.update(activity_params)
-        format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
+        format.html { redirect_to ('/activities'), notice: 'Activity was successfully updated.' }
         format.json { render :show, status: :ok, location: @activity }
       else
         format.html { render :edit }
